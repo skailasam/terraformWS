71e21b4d88c3a31cbd6ddabd813592bee57be95e
lonleyroot <kai.rietchi@gmail.com>
2021-11-19 13:26:59 +0100

    add data source

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
-  ami           = "ami-0cd855c8009cb26ef"
+  ami           = data.aws_ami.amazon-linux-2.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.web.id]
   tags = merge(local.tags, local.customer, tomap({ Name = "Michis Webserver"})) 
@@ -61,5 +61,14 @@ variable "my_default_tags" {
   default = {
     automated_through = "Terraform"
     Terraform = true
+  }
+}
+
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
   }
 }
